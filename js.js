f246d9a36b307b43ba31498cd39e797156f5a7fb
Dr-Mostafa Shhata <77467052+MOSTAFA4799@users.noreply.github.com>
2026-06-07 14:24:31 +0300

    Delete js.js

--- js.js
+++
@@ -1,9 +0,0 @@
-// في الـ JS
-setTimeout(() => {
-  document.getElementById('welcome-overlay').classList.add('visible');
-}, 100);
-
-function dismissWelcome() {
-  const overlay = document.getElementById('welcome-overlay');
-  overlay.classList.add('hidden');
-}
